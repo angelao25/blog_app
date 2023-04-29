eb8b7cec1a0eb835c3b763ce56702be6f29fe4c8
Angel Aguirre Obando <anngelao25@gmail.com>
2023-04-29 12:24:36 -0500

    Comments on Posts working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-#
+
+User.create(
+  email: "dean@example.com",
+  name: "Dean",
+  password: "password",
+  password_confirmation: "password"
+)
+
+User.create(
+  email: "John@doe.com",
+  name: "John",
+  password: "password",
+  password_confirmation: "password"
+)
+
 10.times do |x|
-  Post.create(title: "Title #{x}", body: "Body #{x} Words go here")
+  Post.create(
+    title: "Title #{x}",
+    body: "Body #{x} Words go here",
+    user_id: User.first.id
+  )
 end
